06a8a9838f52b2ad60ba003cc52a593cfa4fdd0e
LucasAmmer <lucasammerlaan123@gmail.com>
2023-11-01 20:08:03 +0100

    added url the way the library does

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -32,7 +32,27 @@ var layers [][]string;
 var currentLayer = 0;
 var disallowed []string;
 const useragent = "decsentCrawler";
-func visit(url string){
+
+type Userinfo struct {
+	username    string
+	password    string
+	passwordSet bool
+}
+
+type URL struct {
+	Scheme      string
+	Opaque      string    // encoded opaque data
+	User        *Userinfo // username and password information
+	Host        string    // host or host:port
+	Path        string    // path (relative paths may omit leading slash)
+	RawPath     string    // encoded path hint (see EscapedPath method)
+	ForceQuery  bool      // append a query ('?') even if RawQuery is empty
+	RawQuery    string    // encoded query values, without '?'
+	Fragment    string    // fragment for references, without '#'
+	RawFragment string    // encoded fragment hint (see EscapedFragment method)
+}
+
+func visit(url *URL){
 	// Visit said url
 }
 
